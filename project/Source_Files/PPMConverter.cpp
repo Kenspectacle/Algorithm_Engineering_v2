a8016c6a12931bc51b6455dc01e109f71a96cb91
Walter Ehrenberger <wehrenberger@salesforce.com>
2023-03-09 16:15:38 +0100

    Refactor: Extracted image import from make to PPMConverter. Fixed hpp paths. Cleaned main.

--- project/Source_Files/PPMConverter.cpp
+++ project/Source_Files/PPMConverter.cpp
@@ -1,10 +1,31 @@
-// Allocate
+#include "Header_Files/PPMConverter.hpp"
 
-    std::vector<std::vector<int>> imageVect(img.rows, std::vector<int>(img.cols, 0));
-    std::vector<std::vector<int>> paddedImageVect(img.rows + 2, std::vector<int>(img.cols + 2, 0));
+#include <iostream>
 
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+void ocv_getImgInformation(std::string imageName, int *numRows, int *numCols)
+{
+    std::string image_path = samples::findFile("static/" + imageName);
+
+    Mat img = imread(image_path, IMREAD_GRAYSCALE);
+
+    if (img.empty())
+    {
+        std::cout << "Could not read the image: " << image_path << std::endl;
+        return 1;
+    }
+    int numRows = img.rows;
+    int numCols = img.cols;
+}
+
+void ocv_importImageToVector(std::vector<std::vector<int>> imageMatrix,
+                             std::vector<std::vector<int>> paddedImageMatrix,
+                             const int *numRows, const int *numCols);
+{
     // Convert from Mat object to 2D Vectors
-
     for (int i = 0; i < img.rows; i++)
     {
         for (int j = 0; j < img.cols; j++)
@@ -15,3 +36,4 @@
             paddedImageVect[i + 1][j + 1] = extracted;
         }
     }
+}
--- project/Source_Files/main.cpp
+++ project/Source_Files/main.cpp
@@ -1,83 +1,28 @@
-#include <iostream>
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
+#include "PPMConverter.hpp"
 #include "ImageFilter.hpp"
 
-#define PAD_SIZE
-
-using namespace cv;
+#include <iostream>
 
 // using std::vector<std::vector<int>> as Vec2i
 
 int main(int argc, const char **argv)
 {
+    // 1. Step import and convert image
+    int *numRows, *numCols;
+    ocv_getImgInformation("name", numRows, numCols);
+    std::vector<std::vector<int>> imageMatrix(*numRows, std::vector<int>(numCols, 0));
+    std::vector<std::vector<int>> paddedImageMatrix(*numRows + 2, std::vector<int>(numCols + 2, 0));
+    ocv_importImageToVector(imageMatrix, paddedImageMatrix, numRows, numCols);
 
-    std::string image_path = samples::findFile("static/input_image_1.jpg");
-    std::cout << "Hello World" << std::endl;
+    // 2. Step transformations
 
-    Mat img = imread(image_path, IMREAD_GRAYSCALE);
+    std::vector<std::vector<int>> blurredImage = gaussianFilter(paddedImageMatrix, *numRows, *numCols);
 
-    // Error handling
-    if (img.empty())
-    {
-        std::cout << "Could not read the image: " << image_path << std::endl;
-        return 1;
-    }
-    int numRows = img.rows;
-    int numCols = img.cols;
+    std::vector<std::vector<int>> filteredImage = customFilter(blurredImage, imageMatrix, *numRows, *numCols);
 
-    // Allocate
+    // 3. Convert image back to ppm
 
-    std::vector<std::vector<int>> imageVect(img.rows, std::vector<int>(img.cols, 0));
-    std::vector<std::vector<int>> paddedImageVect(img.rows + 2, std::vector<int>(img.cols + 2, 0));
+    // 4. Export image
 
-    // Convert from Mat object to 2D Vectors
-
-    for (int i = 0; i < img.rows; i++)
-    {
-        for (int j = 0; j < img.cols; j++)
-        {
-            int extracted = img.at<uchar>(i, j);
-            // std::cout << extracted << std::endl;
-            imageVect[i][j] = extracted;
-            paddedImageVect[i + 1][j + 1] = extracted;
-        }
-    }
-
-    // TO BE REFACTORED!!!
-
-    // for (int i = 1; i)
-
-    // Convert from 2D Array to Mat object
-    int *flat_img = new int[img.rows * img.cols];
-
-    int k = 0;
-    for (int i = 0; i < img.rows; i++)
-    {
-        for (int j = 0; j < img.cols; j++)
-        {
-            flat_img[k] = imageVect[i][j];
-            k++;
-        }
-    }
-
-    Mat A(img.rows, img.cols, CV_8U, flat_img);
-    std::cout << "A cols: " << A.cols << "\n";
-    std::cout << "A rows: " << A.rows << "\n";
-    std::cout << "img cols: " << img.cols << "\n";
-    std::cout << "img rows: " << img.rows << "\n";
-    std::cout << "Squares:" << imageVect[0][2];
-
-    // Clean up when done
-    std::cout << "test line " << __LINE__ << "\n";
-
-    std::cout << "test line " << __LINE__ << "\n";
-
-    delete[] flat_img;
-    std::cout << "test line " << __LINE__ << "\n";
-
-    std::vector<std::vector<int>> blurredImage = gaussianFilter(paddedImageVect, numRows, numCols);
-    std::vector<std::vector<int>> filteredImage = customFilter(blurredImage, imageVect, numRows, numCols);
     return 0;
 }
